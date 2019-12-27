98ae56f34bdeaa3d073a92eb18e1fb9a9908621b
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2019-12-26 19:54:33 -0700

    style in css

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 var searchBtn = $("#searchBtn");
-var restaurantInfo = $("#restaurantInfo");
+var userInput = $("#user-input");
+var restaurantInfo = $("restaurantInfo")
 var restaurants = [];
 
 function displayRestaurantInfo(){
     var restaurant = $(this).attr("data-name");
-    var queryURL = "http://opentable.herokuapp.com/api/restaurants?name=" + restaurant
+    var queryURL = "http://opentable.herokuapp.com/api/restaurants?=" + restaurant
 
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
         console.log(response)
+
         var restaurantDiv = $("<div class='restaurant'>");
 
         restaurantInfo.append(restaurantDiv);
     });
 };
 
-$(searchBtn).on("click", function(event) {
-    event.preventDefault();
-    var resraurant = $("#user-input").val().trim();
-    restaurants.push(restaurant);
+// $(searchBtn).on("click", function(event) {
+//     event.preventDefault();
+//     var restaurant = $("#user-input").val().trim();
+//     restaurant.attr("data-name")
+//     restaurants.push(restaurant);
     
-    displayRestaurantInfo();
-});
+//     displayRestaurantInfo();
+// });
