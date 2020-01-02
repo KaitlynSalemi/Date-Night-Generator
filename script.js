1cb89b72de4ddb63b48f8e459b76ac9e9f61e3a7
LPMartinez <lpmartinez1109@gmail.com>
2020-01-02 13:54:21 -0600

    linked resturant prices to movie arrays

--- script.js
+++ script.js
@@ -3,12 +3,35 @@ $(document).ready(function() {
     var userInput = $("#autocomplete-input");
     var restaurantInfo = $("#restaurant-info");
     var movieInfo = $("#movie-info");
-    var movie = {};
-    function movieGenre(){
+    var movie = "";
+    
+    function movieGenre(price){
       var comedy = ["We're The Millers", "SuperBad", "Life Of Brain", "Animal House", "Hot Fuzz"];
       var adventure = ["Avengers: Endgame", "Jurassic Park", "Casino Royale", "Mission Impossible: Fallout", "John Wick"];
       var biography = ["Hacksaw Ridge", "First Man", "The Social Network", "Bohemian Rhapsody", "Sully"];
       var romance = ["The Notebook", "Titanic", "Forrest Gump", "A Walk To Remember", "Crazy Rich Asians"];
+
+      if (price === 4){
+        var randomNumber = Math.floor(Math.random()* romance.length)
+        movie = romance[randomNumber]
+        displayMovieInfo();
+        
+      }
+      else if (price === 3){
+        var randomNumber = Math.floor(Math.random()* biography.length)
+        movie = biography[randomNumber]
+        displayMovieInfo();
+      }
+      else if (price === 2){
+        var randomNumber = Math.floor(Math.random()* adventure.length)
+        movie = adventure[randomNumber]
+        displayMovieInfo();
+      }
+      else if (price === 1){
+        var randomNumber = Math.floor(Math.random()* comedy.length)
+        movie = comedy[randomNumber]
+        displayMovieInfo();
+      }
     }
 
     function displayRestaurantInfo(){
@@ -21,11 +44,16 @@ $(document).ready(function() {
             method: "GET"
         }).then(function(response) {
             console.log(response)
+            for (var i = 0; i<response.restaurants.length;i++){
+              movieGenre(response.restaurants[i].price);
 
-        });
-    };
-
+            }
+          });
+        };
+        
     function displayMovieInfo(){
+      console.log(movie);
+      
         var movieURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=e379c334";
 
         $.ajax({
@@ -44,7 +72,7 @@ $(document).ready(function() {
         userInput.val().trim();
         
         displayRestaurantInfo();
-        displayMovieInfo();
+       
     });
 
 
