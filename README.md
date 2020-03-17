fbc170d82c3fb0ca9d31cd980d7df6ca1dcdd400
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-03-17 17:50:12 -0500

    cleaned code and readme

--- README.md
+++ README.md
@@ -1,17 +1,33 @@
-# Project-1
+# Date-Night-Generator
 
-For this project, we wanted to create a night out planner for users who might need a little help in planning a night out. By combining APIs from both OpenTable and OMDb, we were able to put together a site that will allow users to type in a resturant from OpenTable and then be treated to a movie suggestion based on the price range of the resturant.
+## Objective
 
-If users want to choose a different movie selection, then they have to option to choose a new movie. Users will also be able to go directly to OpenTable to make reservations via the links that are provide with each of their selections.
+We created this fun app that will help our users plan their night out. Our users can find their resturaunt and a movie to pair with it all in one place. 
+
+## Purpose
+
+Planning a date can be stressful and make you feel indecisive. We wanted to make this application to take some of the pressure off of dating and to Show off our third party API skills.
+
+## Process
+
+To make our App we used 2 third party APIs. OMDb to produce our movie information and OpenTable for our resturaunt information. We also used Materialze to style our application. 
+
+## Tools and Languages
+
+- OMDb API
+- OpenTable API
+- Materialze
+- HTML
+- CSS
+- JavaScript
+- JQuery
+- ajax
 
 ## Goals
 
-In the future, we would like to focus on add a city selection function that would allow users to filter different cities and pull resturants from specific places.
-
-Another focus of ours would be to allow auto scroll for mobile devices. Accomplishing this would allow the users to automatically see their movie selection, regardless of how many resturant options appear on the screen.
-
-We would also like to be able to use different resturants APIs to allow users to access different resturants that don't require reservations and also have delivery options as well.
-
-Regrading the movie APIs, the next step would be to instill filters that would allow users to choose from movies in theaters and movies that are streaming. We would also like to add the options for users to view trailers for the movie while sill on the website. Users will able to watch trailers on a min-player that will display oif the user decides to click on either the images or a movie trailer button.
-
-We would also like to include a mapping element allowing users to see where each of their restaurants are located at.
+* filter by city
+* auto scroll to the movie match
+* addition APIs to include more restaurants 
+* addition APIs to include movies that are currently in theatres or on streaming services
+* add movie trailers
+* add Google maps API for both restaurants and movies. 
--- script.js
+++ script.js
@@ -1,259 +1,188 @@
 
 $(document).ready(function() {
-    var userInput = $("#autocomplete-input");
-    var restaurantInfo = $("#restaurant-info");
-    var movieInfo = $("#movie-info");
-    var movie = "";
+  var userInput = $("#autocomplete-input");
+  var restaurantInfo = $("#restaurant-info");
+  var movieInfo = $("#movie-info");
+  var movie = "";
+  
+  
+  function movieGenre(price){
+    $('.movie-display').empty();
     
-    
-    function movieGenre(price){
-        $('.movie-display').empty();
-        console.log(price);
-        
-      var comedy = ["We're The Millers", "SuperBad", "Life Of Brain", "Animal House", "Hot Fuzz", "Beetlejuice", "Ferris Bueller's Day Off", "The Hangover", "The Princess Bride", "This Is Where I Leave You"];
-      var adventure = ["Avengers: Endgame", "Jurassic Park", "Casino Royale", "Mission Impossible: Fallout", "John Wick", "Jumanji", "Back To The Future", "Spectre", "The Goonies", "The Incredibles"];
-      var biography = ["Hacksaw Ridge", "First Man", "The Social Network", "Bohemian Rhapsody", "Sully", "October Sky", "Dunkirk", "Rocketman", "The Wolf Of Wall Street", "Catch Me If You Can"];
-      var romance = ["The Notebook", "Titanic", "Forrest Gump", "A Walk To Remember", "Crazy Rich Asians", "Dirty Dancing", "Silver Linings Playbook", "When Harry Met Sally", "Pretty Woman", "Breakfast At Tiffany's"];
-      console.log(comedy);
-      console.log(adventure);
-      console.log(biography);
-      console.log(romance);
       
-      if (price == 4){
-        var randomNumber = Math.floor(Math.random()* romance.length)
-        movie = romance[randomNumber]
-        console.log(movie);
-        
-        displayMovieInfo();
-        
-      }
-      else if (price == 3){
-        var randomNumber = Math.floor(Math.random()* biography.length)
-        movie = biography[randomNumber]
-        console.log(movie);
-        displayMovieInfo();
-      }
-      else if (price == 2){
-        var randomNumber = Math.floor(Math.random()* adventure.length)
-        movie = adventure[randomNumber]
-        displayMovieInfo();
-      }
-      else if (price == 1){
-        var randomNumber = Math.floor(Math.random()* comedy.length)
-        movie = comedy[randomNumber]
-        displayMovieInfo();
-      }
+    var comedy = ["We're The Millers", "SuperBad", "Life Of Brain", "Animal House", "Hot Fuzz", "Beetlejuice", "Ferris Bueller's Day Off", "The Hangover", "The Princess Bride", "This Is Where I Leave You"];
+    var adventure = ["Avengers: Endgame", "Jurassic Park", "Casino Royale", "Mission Impossible: Fallout", "John Wick", "Jumanji", "Back To The Future", "Spectre", "The Goonies", "The Incredibles"];
+    var biography = ["Hacksaw Ridge", "First Man", "The Social Network", "Bohemian Rhapsody", "Sully", "October Sky", "Dunkirk", "Rocketman", "The Wolf Of Wall Street", "Catch Me If You Can"];
+    var romance = ["The Notebook", "Titanic", "Forrest Gump", "A Walk To Remember", "Crazy Rich Asians", "Dirty Dancing", "Silver Linings Playbook", "When Harry Met Sally", "Pretty Woman", "Breakfast At Tiffany's"];
+    
+    
+    if (price == 4){
+      var randomNumber = Math.floor(Math.random()* romance.length)
+      movie = romance[randomNumber]
+      
+      
+      displayMovieInfo();
       
     }
-
-    function displayRestaurantInfo(){
-        var restaurant = userInput.val();
-        console.log(restaurant);
-        var queryURL = "https://cors-anywhere.herokuapp.com/opentable.herokuapp.com/api/restaurants?name=" + restaurant
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            $("#restaurant-info").empty();
-            console.log(response);
-            for (var i = 0; i < response.restaurants.length; i++) {
-                // var restaurantName = $("<p>").text(response.restaurants[i].name);
-
-                // restaurantName.attr('class', 'rt')
-                // restaurantName.attr('data-name', response.restaurants[i].name)
-                // restaurantName.attr('data-address', response.restaurants[i].address)
-                // restaurantName.attr('data-city', response.restaurants[i].city)
-                // restaurantName.attr('data-state', response.restaurants[i].state)
-                // restaurantName.attr('data-postal_code', response.restaurants[i].postal_code)
-                // restaurantName.attr('data-phone', response.restaurants[i].phone)
-                // restaurantName.attr('data-price', response.restaurants[i].price)
-                // restaurantName.attr('data-reserve_url', response.restaurants[i].reserve_url)
-                // restaurantName.attr('data-image_url', response.restaurants[i].image_url)
-
-                // restaurantInfo.append(restaurantName);
-
-                // console.log($(this).attr("data-name"));
-                // var rtName = $(this).attr("data-name");
-                // var rtImg = $(this).attr("data-image_url");
-                // var rtAddress = $(this).attr('data-address');
-                // var rtCity = $(this).attr('data-city');
-                // var rtState = $(this).attr('data-state');
-                // var rtPC = $(this).attr('data-postal_code');
-                // var rtPhone = $(this).attr('data-phone');
-                // var rtPrice = $(this).attr('data-price');
-                
-        
-                var row = $("<div>").attr('class', "row");
-                var col = $("<div>")
-                // .attr('class', 'col s12 m7');
-
-                var cardDiv = $("<div>").attr('class', 'card');
-                var cardImage = $("<div>").attr('class', 'card-image');
-                var img = $("<img>");
-                img.attr('src', response.restaurants[i].image_url);
-                var span = $("<span>").attr('class', 'card-title');
-                var content = $("<div>").attr('class', 'card-content');
-                var pAddress = $("<p>");
-                var pCSP = $("<p>");
-                var pPhone = $("<p>");
-                var pPrice = $("<p>");
-                var reserve = $("<p>");
-                var reserveLink = $("<a>");
-                var action = $("<div>").attr('class', 'card-action');
-                var generateMovieBtn = $("<a>").attr('class', 'generate-movie');
-                generateMovieBtn.attr('href', movieInfo);
-        
-                span.text(response.restaurants[i].name);
-                pAddress.text(response.restaurants[i].address);
-                pCSP.text((response.restaurants[i].city)+ " " + (response.restaurants[i].state) + " " +(response.restaurants[i].postal_code));
-                pPhone.text("Phone #: " + (response.restaurants[i].phone));
-                pPrice.text("Price: " + (response.restaurants[i].price));
-                pPrice.attr("data-price",response.restaurants[i].price);
-                pPrice.attr("class", "price");
-                reserve.text("Reserve link: ")
-                reserveLink.text(response.restaurants[i].reserve_url);
-                reserveLink.attr("href", response.restaurants[i].reserve_url);
-                reserveLink.attr("target", "_blank");
-                generateMovieBtn.text("Generate Movie");
-                
-                // $("#restaurant-info").append(restaurant.length);
-        
-        
-                restaurantInfo.append(row);
-                row.append(col);
-                col.append(cardDiv);
-                cardDiv.append(cardImage);
-                cardImage.append(img);
-                content.append(span);
-                cardDiv.append(content);
-                content.append(pAddress);
-                content.append(pCSP);
-                content.append(pPhone);
-                content.append(pPrice);
-                content.append(reserve);
-                reserve.append(reserveLink);
-                cardDiv.append(action);
-                action.append(generateMovieBtn);
-                
-
-            }
-            
-        });
-
-        // <div class ="row">
-        //     <div class="col s12 m7">
-        //         <div class="card">
-        //             <div class="card-image">
-        //                 <img src="images/sample-1.jpg">
-        //                 <span class="card-title">Card Title</span>
-        //             </div>
-        //             <div class="card-content">
-        //                 <p>I am a very simple card. I am good at containing small bits of information.
-        //                 I am convenient because I require little markup to use effectively.</p>
-        //             </div>
-        //             <div class="card-action">
-        //                 <a href="#">This is a link</a>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        
-    };
-
-   
-    function displayMovieInfo(){
-      console.log(movie);
+    else if (price == 3){
+      var randomNumber = Math.floor(Math.random()* biography.length)
+      movie = biography[randomNumber]
       
-        var movieURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=e379c334";
+      displayMovieInfo();
+    }
+    else if (price == 2){
+      var randomNumber = Math.floor(Math.random()* adventure.length)
+      movie = adventure[randomNumber]
+      displayMovieInfo();
+    }
+    else if (price == 1){
+      var randomNumber = Math.floor(Math.random()* comedy.length)
+      movie = comedy[randomNumber]
+      displayMovieInfo();
+    }
+    
+  }
 
-        $.ajax({
-            url: movieURL,
-            method: "GET"
-        }).then(function(movieResponse) {
-            console.log(movieResponse);
-            var row = $("<div>").attr('class', "row");
-            // row.attr('href', "#target")
-            var col = $("<div>")
-            var cardDiv = $("<div>").attr('class', 'card');
+  function displayRestaurantInfo(){
+    var restaurant = userInput.val();
+    
+    var queryURL = "https://cors-anywhere.herokuapp.com/opentable.herokuapp.com/api/restaurants?name=" + restaurant
 
-            // cardDiv.attr("id", "target");
-
-            // var targetting = $('<a>').attr('name', 'theMovie');
-            // cardDiv.append(targetting)
-
-            var cardImage = $("<div>").attr('class', 'card-image');
-            var img = $("<img>");
-            img.attr('src', movieResponse.Poster);
-            // var span = $("<span>").attr('class', 'card-title');
-            var content = $("<div>").attr('class', 'card-content');
-            var pTitle = $("<p>").attr('class', 'movie-title');
-            var pYear = $("<p>");
-            var pRating = $("<p>");
-            var pPlot = $("<p>");
-
-            // span.text(movieResponse.Title);
-            pTitle.text(movieResponse.Title);
-            pYear.text("Year: " + movieResponse.Year);
-            pRating.text("Rating: " + (movieResponse.Rated));
-            pPlot.text(movieResponse.Plot);
-            
-            movieInfo.append(row);
-            row.append(col);
-            col.append(cardDiv);
-            cardDiv.append(cardImage);
-            cardImage.append(img);
-            // cardImage.append(span);
-            cardDiv.append(content);
-            content.append(pTitle, pYear, pRating, pPlot);
-            row.attr('class', 'movie-display')
-            
-
-            // // console.log(movieResponse)
-            // var title = movieResponse.Title;
-            // var pOne = $("<p>").text(title);
-            // movieInfo.append(pOne);
-            // // console.log(movieResponse.Title);
-            // var year = movieResponse.Year;
-            // var pTwo = $("<p>").text(year);
-            // movieInfo.append(pTwo);
-            // // console.log(movieResponse.Year);
-            // var rated = movieResponse.Rated;
-            // var pThree = $("<p>").text(rated);
-            // movieInfo.append(pThree);
-            // // console.log(movieResponse.Rated);
-            // var plot = movieResponse.Plot;
-            // var pFour = $("<p>").text(plot)
-            // movieInfo.append(pFour);
-            // // console.log(movieResponse.Plot);
-            
-            // $("#movie-info").append(pOne, pTwo, pThree, pFour);
-        });
-
-    };
-
-    $(document).on("click", ".generate-movie",function(event) {
-        event.preventDefault();
-
-        // console.log($(this).parent().parent().find("div.card-content").find("p.price").attr("data-price"));
-        var price = $(this).parent().parent().find("div.card-content").find("p.price").attr("data-price");
-
-        movieGenre(price);
-    });
-
-    $("#searchBtn").on("click", function(event) {
-        event.preventDefault();
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      $("#restaurant-info").empty();
+     
+      for (var i = 0; i < response.restaurants.length; i++) {
+       
         
-        userInput.val().trim();
+
+        var row = $("<div>").attr('class', "row");
+        var col = $("<div>")
+       
+
+        var cardDiv = $("<div>").attr('class', 'card');
+        var cardImage = $("<div>").attr('class', 'card-image');
+        var img = $("<img>");
+        img.attr('src', response.restaurants[i].image_url);
+        var span = $("<span>").attr('class', 'card-title');
+        var content = $("<div>").attr('class', 'card-content');
+        var pAddress = $("<p>");
+        var pCSP = $("<p>");
+        var pPhone = $("<p>");
+        var pPrice = $("<p>");
+        var reserve = $("<p>");
+        var reserveLink = $("<a>");
+        var action = $("<div>").attr('class', 'card-action');
+        var generateMovieBtn = $("<a>").attr('class', 'generate-movie');
+        generateMovieBtn.attr('href', movieInfo);
+
+        span.text(response.restaurants[i].name);
+        pAddress.text(response.restaurants[i].address);
+        pCSP.text((response.restaurants[i].city)+ " " + (response.restaurants[i].state) + " " +(response.restaurants[i].postal_code));
+        pPhone.text("Phone #: " + (response.restaurants[i].phone));
+        pPrice.text("Price: " + (response.restaurants[i].price));
+        pPrice.attr("data-price",response.restaurants[i].price);
+        pPrice.attr("class", "price");
+        reserve.text("Reserve link: ")
+        reserveLink.text(response.restaurants[i].reserve_url);
+        reserveLink.attr("href", response.restaurants[i].reserve_url);
+        reserveLink.attr("target", "_blank");
+        generateMovieBtn.text("Generate Movie");
         
-        displayRestaurantInfo();
+       
+
+
+        restaurantInfo.append(row);
+        row.append(col);
+        col.append(cardDiv);
+        cardDiv.append(cardImage);
+        cardImage.append(img);
+        content.append(span);
+        cardDiv.append(content);
+        content.append(pAddress);
+        content.append(pCSP);
+        content.append(pPhone);
+        content.append(pPrice);
+        content.append(reserve);
+        reserve.append(reserveLink);
+        cardDiv.append(action);
+        action.append(generateMovieBtn);
+          
+
+      }
+        
+    });
+  };
+
+  
+  function displayMovieInfo(){
+
+    
+    var movieURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=e379c334";
+
+    $.ajax({
+      url: movieURL,
+      method: "GET"
+    }).then(function(movieResponse) {
+      
+      var row = $("<div>").attr('class', "row");
+     
+      var col = $("<div>")
+      var cardDiv = $("<div>").attr('class', 'card');
+
+
+      var cardImage = $("<div>").attr('class', 'card-image');
+      var img = $("<img>");
+      img.attr('src', movieResponse.Poster);
+      
+      var content = $("<div>").attr('class', 'card-content');
+      var pTitle = $("<p>").attr('class', 'movie-title');
+      var pYear = $("<p>");
+      var pRating = $("<p>");
+      var pPlot = $("<p>");
+
+     
+      pTitle.text(movieResponse.Title);
+      pYear.text("Year: " + movieResponse.Year);
+      pRating.text("Rating: " + (movieResponse.Rated));
+      pPlot.text(movieResponse.Plot);
+      
+      movieInfo.append(row);
+      row.append(col);
+      col.append(cardDiv);
+      cardDiv.append(cardImage);
+      cardImage.append(img);
+      cardDiv.append(content);
+      content.append(pTitle, pYear, pRating, pPlot);
+      row.attr('class', 'movie-display')
+
     });
 
-    userInput.keypress(function (event) {
-        if (event.which === 13) {
-            displayRestaurantInfo();
-        }
-    });
+  };
+
+  $(document).on("click", ".generate-movie",function(event) {
+    event.preventDefault();
+
+    var price = $(this).parent().parent().find("div.card-content").find("p.price").attr("data-price");
+
+    movieGenre(price);
+  });
+
+  $("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    
+    userInput.val().trim();
+    
+    displayRestaurantInfo();
+  });
+
+  userInput.keypress(function (event) {
+    if (event.which === 13) {
+      displayRestaurantInfo();
+    }
+  });
 
 });
 
